cddd297ccbbce46417d954c90895ce0c919e812a
pofvictor <pofvictorz@yahoo.com>
2024-07-04 07:20:58 -0500

    Add fns_and_dsa directory and its contents

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -6,13 +6,10 @@ def perform_operation(num1, num2, operation):
     elif operation == 'multiply':
         return num1 * num2
     elif operation == 'divide':
-        if num2 != 0:
-            return num1 / num2
-        else:
+        if num2 == 0:
             print("Cannot divide by zero.")
             return None
+        else:
+            return num1 / num2
     else:
-        return f"Invalid operation '{operation}'. Please choose from '+', '-', '*', '/'."
-
-
-
+        return f"Invalid operation '{operation}'. Please choose from 'add', 'subtract', 'multiply', 'divide'."
